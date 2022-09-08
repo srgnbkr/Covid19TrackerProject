7633df48f78fc09e7963af854ffba5436b836e52
Sergen Bakır <cuseg09@gmail.com>
2022-09-08 11:13:50 +0300

    Page Title Router Eklendi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NotFoundComponent } from './sharedComponents/not-found/not-found.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import {
+  RouterModule,
+  RouterStateSnapshot,
+  Routes,
+  TitleStrategy,
+} from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
+    title: 'Dünya Geneli Covid19 Verileri',
     loadChildren: () =>
       import('./modules/main/main.module').then((m) => m.MainModule),
   },
   {
     path: 'tables',
+    title: 'Ülkeler Geneli Covid19 Verileri',
     loadChildren: () =>
       import('./modules/table-list/table-list.module').then(
         (m) => m.TableListModule
@@ -19,13 +27,34 @@ const routes: Routes = [
   {
     //Wild Card Route for 404 request
     path: '**',
-    pathMatch:'full',
+    pathMatch: 'full',
+    title: 'Sayfa Bulunamadı',
     component: NotFoundComponent,
   },
 ];
 
+@Injectable()
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`${title}`);
+    }
+  }
+}
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [
+    {
+      provide: TitleStrategy,
+      useClass: TemplatePageTitleStrategy,
+    },
+  ],
 })
 export class AppRoutingModule {}
--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'',pathMatch:'full',component:HomePageComponent}
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomePageComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {}
